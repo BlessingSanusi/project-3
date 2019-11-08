63180b2ea503d6404f3ddb977b0590050aac075c
Blessing Sanusi <sanusi.temitope@ymail.com>
2019-11-08 17:16:12 -0600

    adding files

---
+++ README.md
@@ -0,0 +1,21 @@
+# Personal Finance App
+A full stack Mern App that tracks all your transaction from your bank account
+
+
+## Overview
+
+* Link bank account into the app
+* Pulls all transaction.
+* Categories the transaction and show it on a dashboard 
+
+
+## Technology Used
+
+* MongoDB for darabase
+* Express & Node for backend
+* React for front end
+* Redux for state management
+* Chartjs for dashboard ui
+
+## Api
+ * Plaid
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+// const routes = require("./routes/api");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const passport = require("passport");
+const users = require("./routes/api/users");
+const plaid = require("./routes/api/plaid");
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+// app.use(routes);
+
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+
+//   const {
+//     receivePublicToken,
+//     getTransactions
+//     } = require("./controllers/controller");
+
+//     // Get the public token and exchange it for an access token
+// app.post("/auth/public_token", receivePublicToken);
+// // Get Transactions
+// app.get("/transactions", getTransactions);
+  
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+  // Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+app.use("/api/plaid", plaid);
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
